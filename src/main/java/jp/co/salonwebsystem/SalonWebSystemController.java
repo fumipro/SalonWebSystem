af8098126bacf9c79882359f5cd6b9135bc81916
fumipro <fumiprog@gmail.com>
2023-05-30 15:56:56 +0900

    サロンサイトv1.1

--- src/main/java/jp/co/salonwebsystem/SalonWebSystemController.java
+++ src/main/java/jp/co/salonwebsystem/SalonWebSystemController.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
